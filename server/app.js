cc83ca9634956daf6351845e564d3be63941866e
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-10-25 15:46:56 -0700

    finished

--- server/app.js
+++ server/app.js
@@ -5,5 +5,16 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
-const port = 5000;
+app.use((req, res, next) => {
+  const error = new Error("Sorry, the requested resource couldn't be found");
+  error.statusCode = 404;
+  next(error);
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500);
+  res.send({error: err.message || "Internal Server Error"});
+});
+
+const port = 3001;
 app.listen(port, () => console.log('Server is listening on port', port));
